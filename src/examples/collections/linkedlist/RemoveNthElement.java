ab0117f46b3a9a7be7f2112bedcaac5ab07f817a
Siva M <sivam@imac.lan>
2018-11-25 20:50:53 -0800

    added 3 sum problem - though not complete

---
+++ src/examples/collections/linkedlist/RemoveNthElement.java
@@ -0,0 +1,77 @@
+package examples.collections.linkedlist;
+
+import examples.collections.linkedlist.AdditionSample1.ListNode;
+
+public class RemoveNthElement {
+
+	static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+	static ListNode removeNthFromEnd(ListNode head, int n) {
+
+		ListNode slowPointer = head;
+		ListNode resultNode = null;
+		ListNode resultHead = resultNode;
+		
+		if(n <= 0)
+			return head;
+
+		int i = 0;
+
+		while (head != null) {
+			if (i >= n) {
+				if (resultNode == null) {
+					resultNode = new ListNode(slowPointer.val);
+					resultHead = resultNode;
+				} else {
+					resultNode.next = new ListNode(slowPointer.val);
+					resultNode = resultNode.next;
+				}
+				slowPointer = slowPointer.next;
+			}
+
+			head = head.next;
+			i++;
+		}
+		slowPointer = slowPointer.next;
+
+		while (slowPointer != null) {
+			if (resultNode == null) {
+				resultNode = new ListNode(slowPointer.val);
+				resultHead = resultNode;
+			} else {
+				resultNode.next = new ListNode(slowPointer.val);
+				resultNode = resultNode.next;
+			}
+			slowPointer = slowPointer.next;
+		}
+
+		return resultHead;
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ListNode n1 = new ListNode(1);
+		n1.next = new ListNode(2);
+		n1.next.next = new ListNode(3);
+		n1.next.next.next = new ListNode(4);
+		n1.next.next.next.next = new ListNode(5);
+
+		ListNode result = removeNthFromEnd(n1, 6);
+
+		while (result != null) {
+			System.out.println(result.val);
+			result = result.next;
+		}
+
+	}
+
+}
