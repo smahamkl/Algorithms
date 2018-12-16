96fa77989ac4bd7e35d33c748321924956427bb8
smahamkl <tzones2006@gmail.com>
2018-12-15 23:27:32 -0800

    working on valid parenthesis problem

--- src/examples/practice/Permutations.java
+++ src/examples/practice/Permutations.java
@@ -3,27 +3,28 @@ package examples.practice;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 public class Permutations {
-	
-	static List<List<Integer>> l1 = new ArrayList<>();
-	
-    static List<List<Integer>> permute(int[] nums) {
-    	l1.clear();
-    	
-        return permutate(nums, 0, nums.length-1);
-    }
 
-	static List<List<Integer>> permutate(int[] arr, int l, int r) {
+	static List<List<String>> l1 = new ArrayList<>();
 
-		
+	static List<List<String>> permute(String[] nums) {
+		l1.clear();
+
+		return permutate(nums, 0, nums.length - 1);
+	}
+
+	static List<List<String>> permutate(String[] arr, int l, int r) {
 
 		if (l == r) {
-			printArr(arr);
-			ArrayList<Integer> l2 = new ArrayList<>();
-			for (int i : arr)
-				l2.add(i);
-			l1.add(l2);
+			if (validateBrackets(arr)) {
+				printArr(arr);
+				ArrayList<String> l2 = new ArrayList<>();
+				for (String i : arr)
+					l2.add(i);
+				l1.add(l2);
+			}
 
 		} else {
 			for (int i = l; i <= r; i++) {
@@ -37,22 +38,41 @@ public class Permutations {
 
 	}
 
-	static void swapElement(int[] arr, int l, int r) {
-		int tmp = arr[l];
+	static void swapElement(String[] arr, int l, int r) {
+		String tmp = arr[l];
 		arr[l] = arr[r];
 		arr[r] = tmp;
 	}
 
-	static void printArr(int[] arr) {
-		for (int i : arr) {
+	static void printArr(String[] arr) {
+		for (String i : arr) {
 			System.out.print(i + " ");
 		}
 
 		System.out.println();
 	}
 
+	static boolean validateBrackets(String[] arr) {
+		boolean result = true;
+
+		Stack<String> s = new Stack<>();
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == "(")
+				s.push(arr[i]);
+			else if (arr[i] == ")") {
+				if (s.isEmpty() || s.peek() == null)
+					return false;
+				else
+					s.pop();
+			}
+		}
+
+		return result;
+	}
+
 	public static void main(String[] args) {
-		int[] arr = new int[] { 1, 2, 3, 4 };
+		String[] arr = new String[] { "(", "(", ")", ")" };
 		permute(arr);
 	}
 
---
+++ src/examples/practice/ValidParenthesis.java
@@ -0,0 +1,80 @@
+package examples.practice;
+
+import java.util.Stack;
+
+public class ValidParenthesis {
+
+	static boolean validateBrackets(String arr, int l, int r) {
+		boolean result = true;
+
+		Stack<Character> s = new Stack<>();
+
+		for (int i = l; i <= r; i++) {
+			if (arr.charAt(i) == '(')
+				s.push(arr.charAt(i));
+			else if (arr.charAt(i) == ')') {
+				if (s.isEmpty() || s.peek() == null)
+					return false;
+				else {
+					s.pop();
+				}
+			}
+		}
+
+		return s.empty() ? result : false;
+	}
+
+	// brut force way...o(n 3) complexity
+
+	static int longestParenthesis(String arr) {
+		int totalLength = 0;
+
+		for (int i = 0; i < arr.length() - 1; i++) {
+			for (int j = arr.length() - 1; j > i; j--) {
+				if (arr.charAt(i) != ')' && arr.charAt(j) != '(' && totalLength < (j - i + 1)) {
+					if (validateBrackets(arr, i, j)) {
+						totalLength = Math.max(totalLength, (j - i + 1));
+						break;
+					}
+				}
+			}
+		}
+
+		return totalLength;
+	}
+	
+	//debug the program and check how the size is compiled for each iteration of i
+	//
+
+	static int longestValidParentheses(String s) {
+		int maxans = 0;
+		int dp[] = new int[s.length()];
+		for (int i = 1; i < s.length(); i++) {
+			if (s.charAt(i) == ')') {
+				if (s.charAt(i - 1) == '(') {
+					dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+				} else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+					dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+				}
+				maxans = Math.max(maxans, dp[i]);
+			}
+		}
+		return maxans;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s = "()()((((()))";
+
+		s = "()()()(()))()()())))((()()()(()()))(()()()((()()))())(((())()())(()(()(()(())(((()())()))(()))()))))()())(()))))()()(())()()((())()()))))((()))))(()()((()(()(()())((())()()()()))()()()(())()()())((((()(())())))(((()(((()((((())())(()()()()(((((()))()(()(())))(((()()()()(()(((())(()(()()(()(()(())()))))))()))()())((()((((()(())(()()()(((((()())()))))())))((((()()()(()(())(((())(((()()((()((()(((()(()))(((())(((()((((()(())(((()((()(()())))))(()(()()(())))))()(()()((()))()))())())((())))()(())((((()((()))))()())()))((()(()())()())()()((()))())(()(()(())((((((()()((((())))())(((()()())))()(((()(()()((((())))))()()((((()(()()())(()(())()()()((()(()((((())))((((((())(()())()))))(()(()))()))))))(()()((()()()())(())))(((()))(()()()(()))((())()()()())()()(((())(()(())()()(()())((()()(((((())(()((((()(()()()(()))(()((((())()())()())())))())(((()(()((())()()((((()()()()))))))())())()(((((()())()(()()()()()(((())((((((()))(())()(()())(()(()())(()(())))())))(()()(()((((()()(())(()))()))(()))(()())())()))))))()()(())))))()))()(()())))((())(()()))()((()))()(())()()((((())()))((()(()))()(((()())()(()()((()())((((())()))))()(())())())())(((()(()())))((()))))()())))))(()((())))()())((()))()()))(())())()))()()((()(((())()()()((()()(()(())(()))())())(((()())(()())(()((()()()())()(()(((((((()()(((()(((((((())(()))))())()))))))))()(()(()((((())(()()(((()))()(())(((((((((()(()())())()(((()((()(((()(()()(()))(())))))))(((";
+
+		// System.out.println(validateBrackets(s, 0, s.length() - 1));
+
+		// System.out.println(longestParenthesis(s));
+
+		System.out.println(longestValidParentheses(s));
+
+	}
+
+}
