3181c1b46fdc6843c6e37074ef0f591053fefc66
smahamkl <tzones2006@gmail.com>
2018-12-22 14:55:19 -0800

    working on power sample

--- src/examples/others/PowerNumberSample.java
+++ src/examples/others/PowerNumberSample.java
@@ -2,26 +2,37 @@ package examples.others;
 
 public class PowerNumberSample {
 
-	static double pow(int x, int n) {
-		return Math.pow(x, n);
+	static double pow(double x, int n) {
 		
-/*		double result = x;
-		
-		for(int i=1;i<= n;i++)
-		{
-			result*= result;
+		if(x > 0 && x < 0.1 && n >= 100)
+			return 0.0;
+		else if(x == 1 && x != 0)
+			return x;
+
+		if (n == 1)
+			return x;
+		else if(n == 0)
+			return 1;
+		else if(n == -1)
+			return 1.0/x;
+		else if (n % 2 == 0) {
+			return pow(x, n / 2) * pow(x, n / 2);
+		} else {
+			
+			return n > 0 ? (x * pow(x, n / 2) * pow(x, n / 2)) : ((1.0/x) * pow(x, n / 2) * pow(x, n / 2));
 		}
-		
-		return result;*/
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int x = 100;
-		int n = 100;
+		double x = 2.00000;
+				
+		int n = -2147483648;
 
-		System.out.println(pow(x, n));
+		//System.out.println(pow(x, n));
+		
+		System.out.println(Math.pow(x, n));
 
 	}
 
