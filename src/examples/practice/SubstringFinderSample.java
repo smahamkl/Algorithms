60732542aa546c7c859dc5af5e65ae6404aa88d8
smahamkl <tzones2006@gmail.com>
2018-12-22 19:00:13 -0800

    working on substring sequence sample

---
+++ src/examples/practice/SubstringFinderSample.java
@@ -0,0 +1,56 @@
+package examples.practice;
+
+import java.util.ArrayList;
+
+public class SubstringFinderSample {
+
+	static int[] subStr;
+	
+	//one idea is to take every substring of s and check if its an anagram of concatenated string of strList
+
+	static void subStringFinder(String s, String[] strList) {
+
+		for (String str : strList) {
+			int l = 0;
+			while (s.substring(l).contains(str)) {
+				int idx = s.indexOf(str, l);
+				subStr[idx] = idx + str.length();
+				l = idx + str.length();
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "barfoofoobarthefoobarman";
+		String[] words = new String[] { "bar", "foo", "the" };
+
+		subStr = new int[s.length()];
+
+		subStringFinder(s, words);
+
+		ArrayList<Integer> finalResult = new ArrayList<>();
+
+		for (int i = 0; i < subStr.length; i++) {
+			if (subStr[i] > 0) {
+				int tmp = subStr[i];
+				int counter = 1;
+				while (tmp < s.length() && subStr[tmp] > 0) {
+					counter++;
+					tmp = subStr[tmp];
+				}
+
+				if (counter == words.length)
+					finalResult.add(i);
+
+				System.out.println(i + " " + subStr[i]);
+			}
+
+		}
+
+		System.out.println("Final list size :" + finalResult.size());
+
+	}
+
+}
